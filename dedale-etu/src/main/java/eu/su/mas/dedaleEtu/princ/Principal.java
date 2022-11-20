0829686b9433ff9f9eed0578a1c2b436392dc78e
Mario Rosas <mario_netta12@hotmail.com>
2022-11-20 01:46:39 +0100

    Adding explorer, collector and tanker agents (low-level) with @Natalia-SP

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/Principal.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/princ/Principal.java
@@ -374,20 +374,20 @@ public class Principal {
 		/*********
 		 * AGENT Explo3
 		 *********/
-		//		//1) Get the container where the agent will appear
-		//		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
-		//		Assert.assertNotNull("This container does not exist",c);
-		//		
-		//		//2) Give the name of your agent, MUST be the same as the one given in the entities file.
-		//		agentName="Explo3";
-		//		
-		//		//3) If you want to give specific parameters to your agent, add them here
-		//		Object [] entityParametersExplo3={"My parameters"};
-		//		
-		//		//4) Give the class name of your agent to let the system instantiate it
-		//		ag=createNewDedaleAgent(c, agentName, DummyMovingAgent.class.getName(), entityParametersExplo3);//ExploreSoloAgent
-		//		
-		//		agentList.add(ag);
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Explo3";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersExplo3={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, ExploreCoopAgent.class.getName(), entityParametersExplo3);//ExploreSoloAgent
+
+				agentList.add(ag);
 
 		/*********
 		 * AGENT Explo4
@@ -445,24 +445,80 @@ public class Principal {
 				ag=createNewDedaleAgent(c, agentName, DummyCollectorAgent.class.getName(), entityParametersC);
 				agentList.add(ag);
 
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Collect2";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersC2={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, DummyCollectorAgent.class.getName(), entityParametersC2);
+				agentList.add(ag);
+
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Collect3";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersC3={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, DummyCollectorAgent.class.getName(), entityParametersC3);
+				agentList.add(ag);
+
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Collect4";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersC4={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, DummyCollectorAgent.class.getName(), entityParametersC4);
+				agentList.add(ag);
+
 		/***************
 		 * AGENT Tanker
 		 ***************/
-		//		
-		//		//1) Get the container where the agent will appear
-		//		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
-		//		Assert.assertNotNull("This container does not exist",c);
-		//		
-		//		//2) Give the name of your agent, MUST be the same as the one given in the entities file.
-		//		agentName="Tanker1";
-		//		
-		//		//3) If you want to give specific parameters to your agent, add them here
-		//		Object [] entityParametersT={"My parameters"};
-		//		
-		//		//4) Give the class name of your agent to let the system instantiate it
-		//		ag=createNewDedaleAgent(c, agentName, DummyTankerAgent.class.getName(), entityParametersT);
-		//		agentList.add(ag);
-		//		
+
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Tanker1";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersT1={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, DummyTankerAgent.class.getName(), entityParametersT1);
+				agentList.add(ag);
+
+				//1) Get the container where the agent will appear
+				c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
+				Assert.assertNotNull("This container does not exist",c);
+
+				//2) Give the name of your agent, MUST be the same as the one given in the entities file.
+				agentName="Tanker2";
+
+				//3) If you want to give specific parameters to your agent, add them here
+				Object [] entityParametersT2={"My parameters"};
+
+				//4) Give the class name of your agent to let the system instantiate it
+				ag=createNewDedaleAgent(c, agentName, DummyTankerAgent.class.getName(), entityParametersT2);
+				agentList.add(ag);
+
 
 		//1) Get the container where the agent will appear
 		//		c = containerList.get(ConfigurationFile.LOCAL_CONTAINER2_NAME);
