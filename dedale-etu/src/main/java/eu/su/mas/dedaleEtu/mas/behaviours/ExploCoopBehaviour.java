749e829dd659251f29298c7e2ffb47f195d544ef
Ejbejaranos <91475734+EjbejaranosAI@users.noreply.github.com>
2023-01-08 14:39:52 +0000

    SendPathBehaviour created

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ExploCoopBehaviour.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ExploCoopBehaviour.java
@@ -121,7 +121,7 @@ public class ExploCoopBehaviour extends SimpleBehaviour {
 					//no directly accessible openNode
 					//chose one, compute the path and take the first step.
 					nextNode=this.myMap.getShortestPathToClosestOpenNode(myPosition).get(0);//getShortestPath(myPosition,this.openNodes.get(0)).get(0);
-					//System.out.println(this.myAgent.getLocalName()+"-- list= "+this.myMap.getOpenNodes()+"| nextNode: "+nextNode);
+					System.out.println(this.myAgent.getLocalName()+"-- list= "+this.myMap.getOpenNodes()+"| nextNode: "+nextNode);
 				}else {
 					//System.out.println("nextNode notNUll - "+this.myAgent.getLocalName()+"-- list= "+this.myMap.getOpenNodes()+"\n -- nextNode: "+nextNode);
 				}
